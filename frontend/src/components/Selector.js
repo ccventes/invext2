146b8257a05d64b7601422ccb60fd4914c4c0b11
christian ventes <chrventes@gmail.com>
2024-06-12 21:31:22 -0500

    Continuando con el selector

--- frontend/src/components/Selector.js
+++ frontend/src/components/Selector.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import "./unal.css";
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import classNames from 'classnames';
+import { Button } from 'react-bootstrap';
 
 
 const Box = ({ children }) => {
   return <div className = "boxStyle">
 
-        <div class="elevated-box">
-             {children}
-        </div>
+        
+            <Row>
+                <h1> Prácticas y Pasantías</h1>
+                <p> Si estás en los últimos semestres de tu pregrado, posgrado o cumples con el 80% disciplinar del avance de tu programa esta información es para ti </p>
+            </Row>
+            <div className='options'>
+                <div className= {classNames('normal-box', 'option2',)}>
+                    
+                    <div className='option-espacio-texto'>
+                        <h2> ¿Cómo realizo mi inscripción a las prácticas?</h2>
+                        <p>De acuerdo a la modalidad se asignará un formato para el diligenciamiento. Recuerda que tenemos 3 modalidades</p>
+
+                    </div>
+                    <Button className="w-100 custom-button">
+                              Botón Personalizado
+                    </Button>
+                    
+        
+                    
+                </div>
+                <div className= {classNames('normal-box', 'option1',)}>
+                    
+                    
+                    {children}
+                </div>
+            </div>
   
   
   </div>;
