6f946fd7f582d0fcc6f950bb607ee23a00dbf57c
christian ventes <chrventes@gmail.com>
2024-07-20 17:36:21 -0500

    creating the video grid

--- frontend/src/components/videos.js
+++ frontend/src/components/videos.js
@@ -7,12 +7,12 @@ export default function Videos() {
   return (
 
     <div className = 'video-grid'>
-            <div style={{ width: '45%', height: 'auto' }}>
-                <Ratio aspectRatio="16x9">
+            <div style={{ width: '45%', height: 'auto', display:'flex',flexDirection:'column' }}>
+                <h1 style ={{minHeight: '100px'}}> La gestión de la propiedad intelectual en la Sede Palmira </h1>
+                <Ratio style = {{boxShadow: '10px 10px gray'}} aspectRatio="16x9">
                         <iframe
-                            src="https://www.youtube.com/embed/uHCEManCMM0"
+                            src="https://www.youtube.com/embed/XpPSW7FKrYI"
                             title="We Binged Dragon Ball Z Abridged: Cell Saga"
-                            frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             referrerPolicy="strict-origin-when-cross-origin"
                             allowFullScreen
@@ -21,11 +21,11 @@ export default function Videos() {
             </div>
             
             <div style={{ width: '45%', height: 'auto' }}>
-                <Ratio aspectRatio="16x9">
+                <h1 style ={{minHeight: '100px'}}>¿Qué son los derechos de autor y derechos conexos? </h1>
+                <Ratio style = {{boxShadow: '10px 10px gray'}} aspectRatio="16x9">
                         <iframe
-                            src="https://www.youtube.com/embed/uHCEManCMM0"
+                            src="https://www.youtube.com/embed/bqy-sGrtszo"
                             title="We Binged Dragon Ball Z Abridged: Cell Saga"
-                            frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             referrerPolicy="strict-origin-when-cross-origin"
                             allowFullScreen
---
+++ frontend/src/pages/item.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function item() {
+  const data ={
+
+    pagina: '/propiedad',
+       
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
